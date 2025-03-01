12d4e5cd14333a134e53cbe059916f7ca53bbcdb
ShakibProgrammar <shakib501886@gmail.com>
2025-03-01 11:22:09 +0600

    updated

--- lib/domain/entities/color.dart
+++ lib/domain/entities/color.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
 class RColors{
-  static const Color buttonColor = Color(0xff21469E);
+  static const Color blueButtonColor = Color(0xff21469E);
+  static const Color blackButtonColor = Color(0xff273238);
 }
