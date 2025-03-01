bb2328b0cfcf84a0b24129b6650dbb4a7ff3204e
mdtayobali13 <mdtayobalideveloper@gmail.com>
2025-03-01 11:58:46 +0600

    sing up

--- lib/domain/entities/color.dart
+++ lib/domain/entities/color.dart
@@ -1,6 +1,13 @@
 import 'dart:ui';
 
+<<<<<<< HEAD
 class RColors {
   static const Color blueButtonColors = Color(0xff21469E);
   static const Color blackButtonColor1 = Color(0xff273238);
 }
+=======
+class RColors{
+  static const Color buttonColor = Color(0xff21469E);
+  static const Color blockButtonColor = Color(0xff273238);
+}
+>>>>>>> de615cd (sing up)
