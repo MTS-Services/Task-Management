fa711e92d8b9be168283dd4e7fbf678ab1ff1a8a
mdtayobali13 <mdtayobalideveloper@gmail.com>
2025-03-10 08:43:07 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,6 @@ import 'package:get/get.dart';
 import 'package:maktrack/domain/entities/color.dart';
 import 'package:maktrack/presentation/pages/screen/bottomNavBar/bottom_nav_bar.dart';
 
-
-
-
-
-void main() {
-  runApp(const MyApp());
-
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
